6325d7cf7519173dbcbac32882f7886f5b1e0738
ryansummerlin <ryansummerlin777@gmail.com>
2023-02-20 15:14:39 -0800

    Finished part 1

--- social.js
+++ social.js
@@ -8,23 +8,44 @@ class SocialNetwork {
   }
 
   addUser(name) {
-    // Your code here
+    this.currentID++;
+    let user = {id: this.currentID, name: name };
+    this.users[this.currentID] = user;
+    this.follows[this.currentID] = new Set();
+
+    return this.currentID;
   }
 
   getUser(userID) {
-    // Your code here
+    if (this.users[userID]) {
+      return this.users[userID];
+    } else {
+      return null;
+    }
   }
 
   follow(userID1, userID2) {
-    // Your code here
+    if (this.users[userID1] && this.users[userID2]) {
+      this.follows[userID1].add(userID2);
+      return true;
+    } else {
+      return false;
+    }
   }
 
   getFollows(userID) {
-    // Your code here
+    return this.follows[userID];
   }
 
   getFollowers(userID) {
-    // Your code here
+    let keys = Object.keys(this.follows);
+    let followers = new Set();
+    keys.forEach(key => {
+      if (this.follows[key].has(userID)) {
+        followers.add(parseInt(key));
+      }
+    });
+    return followers;
   }
 
   getRecommendedFollows(userID, degrees) {
